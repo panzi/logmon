3e2c2d6f3dd66c6c93e7eaced06d8aac9d49a2cd
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-12-17 04:20:35 +0100

    a very basic test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,22 @@
-from typing import Self, Optional
+from typing import Optional
 
 import os
 import sys
 import pytest
-import threading
 
 from os.path import join as join_path
-from pytest_mock import MockerFixture
 from tempfile import gettempdir
 from pathlib import Path
-from email.message import EmailMessage
 from time import sleep
+from subprocess import Popen, PIPE
 
 SRC_PATH = str(Path(__file__).resolve().parent.parent)
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
 
-import logmon
+def indent(text: str, width: int=4) -> str:
+    prefix = ' ' * width
+    return '\n'.join(prefix + line for line in text.split('\n'))
 
 @pytest.fixture(scope="function")
 def temp_prefix(request: pytest.FixtureRequest) -> tuple[str, str]:
@@ -27,89 +27,54 @@ def temp_prefix(request: pytest.FixtureRequest) -> tuple[str, str]:
     return tempdir, f'logmon.test.{PID}.{func_name}'
 
 @pytest.fixture(scope="function")
-def logfiles(temp_prefix: tuple[str, str], count: int = 3) -> list[str]:
+def logfiles(temp_prefix: tuple[str, str], count: int = 3):
     tempdir, prefix = temp_prefix
 
-    return [
+    paths = [
         join_path(tempdir, f'{prefix}.logfile{i}.log')
         for i in range(1, count + 1)
     ]
 
+    yield paths
+
+    for path in paths:
+        if os.path.exists(path):
+            print(f"{path}: was not deleted", file=sys.stderr)
+
 @pytest.fixture(scope="function")
-def logmonrc_path(temp_prefix: tuple[str, str]) -> str:
+def logmonrc_path(temp_prefix: tuple[str, str]):
     tempdir, prefix = temp_prefix
 
-    return join_path(tempdir, f'{prefix}.logmonrc.yaml')
+    path = join_path(tempdir, f'{prefix}.logmonrc.yaml')
+
+    yield path
+
+    if os.path.exists(path):
+        print(f"{path}: was not deleted", file=sys.stderr)
 
 def write_file(filepath: str, contents: str) -> None:
   path = Path('.').joinpath(filepath)
   path.parent.mkdir(parents=True, exist_ok=True)
   path.write_text(contents)
 
-MockSmtpMap = dict[tuple[str, int], "MockSMTP"]
-
-_mock_smtps_instances: MockSmtpMap = {}
-_mock_smtps_instances_lock = threading.Lock()
-
-class MockSMTP:
-    host: str
-    port: int
-    messages: list[EmailMessage]
-
-    def __new__(cls: type[Self], host: str = '', port: int = 0) -> "MockSMTP":
-        with _mock_smtps_instances_lock:
-            key = (host, port)
-            self = _mock_smtps_instances.get(key)
-            if self is None:
-                self = super().__new__(cls)
-                self.__init__(host, port)
-                _mock_smtps_instances[key] = self
-        return self
-
-    def __init__(self, host: str = '', port: int = 0) -> None:
-        self.host = host
-        self.port = port
-        self.messages = []
-
-    def login(self, user: str, password: str, *, initial_response_ok: bool = True):
-        return 235, b''
-
-    def send_message(self, msg: EmailMessage):
-        self.messages.append(msg)
-        return {}
-
-    def close(self):
-        pass
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.close()
-
-@pytest.fixture(scope="function")
-def mock_smtp_map(mocker: MockerFixture):
-    global _mock_smtps_instances
-    _mock_smtps_instances.clear()
-    mocker.patch("smtplib.SMTP", MockSMTP)
-    return _mock_smtps_instances
-
-def test_simple(logmonrc_path: str, logfiles: list[str], mock_smtp_map: MockSmtpMap, mocker: MockerFixture):
-    mocker.patch("signal.signal", lambda sig, handler: None)
-
+def test_simple(logmonrc_path: str, logfiles: list[str]):
+    sender = "alice@example.com"
+    receivers = ["bob@example.com", "charly@example.com"]
     logmonrc = f'''\
 ---
 email:
   protocol: SMTP
   port: 587
   host: localhost
-  sender: alice@example.com
+  sender: "{sender}"
   receivers:
-  - bob@example.com
-  - charly@example.com
+  - "{receivers[0]}"
+  - "{receivers[1]}"
 default:
   use_inotify: true
   seek_end: true
+log:
+  format: "%(message)s"
 logfiles:
   "{logfiles[0]}": {{}}
   "{logfiles[1]}": {{}}
@@ -119,74 +84,115 @@ logfiles:
 '''
     write_file(logmonrc_path, logmonrc)
 
-    thread_exc: Optional[Exception] = None
+    proc = Popen(
+        [sys.executable, join_path(SRC_PATH, 'logmon.py'), '--config', logmonrc_path, '--logmails=instead'],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    assert proc.stdout is not None
+    assert proc.stderr is not None
 
-    def thread_func():
-        nonlocal thread_exc
-        try:
-            logmon.main(['--config', logmonrc_path])
-        except Exception as exc:
-            thread_exc = exc
-
-    thread = threading.Thread(target=thread_func)
-    thread.start()
-    assert thread.ident is not None
-
-    sleep(0.2)
-    if thread_exc is not None:
-        raise thread_exc
+    sleep(0.5)
 
     with open(logfiles[0], 'w') as fp1:
         print("[2025-12-14T20:15:00+0100] INFO: Info message.", file=fp1); fp1.flush()
         print("[2025-12-14T20:16:00+0100] INFO: Info message.", file=fp1); fp1.flush()
-        print("[2025-12-14T20:17:00+0100] ERROR: Something failed!", file=fp1); fp1.flush()
-        print("[2025-12-14T20:18:00+0100] ERROR: Something else failed!", file=fp1); fp1.flush()
+        errmsg1hdr = "ERROR: Something failed!"
+        errmsg2hdr = "ERROR: Something else failed!"
+        errmsg1 = f"[2025-12-14T20:17:00+0100] {errmsg1hdr}"
+        errmsg2 = f"[2025-12-14T20:18:00+0100] {errmsg2hdr}"
+        print(errmsg1, file=fp1); fp1.flush()
+        print(errmsg2, file=fp1); fp1.flush()
         print("[2025-12-14T20:19:00+0100] INFO: Ok again.", file=fp1); fp1.flush()
 
-    sleep(0.2)
-    if thread_exc is not None:
-        raise thread_exc
+    status: Optional[int] = proc.returncode
+    if status is not None and status != 0:
+        stdout = proc.stdout.read()
+        stderr = proc.stderr.read()
+
+        sys.stdout.buffer.write(stdout)
+        sys.stdout.flush()
+
+        sys.stderr.buffer.write(stderr)
+        sys.stderr.flush()
+
+        assert status == 0
 
     with open(logfiles[1], 'w') as fp2:
-        print("[2025-12-14T20:16:00+0100] ERROR: Starts with an error!", file=fp2); fp2.flush()
-        print("Which is actually multi line!", file=fp2); fp2.flush()
+        errmsg3hdr = "ERROR: Starts with an error!"
+        errmsg3_1 = f"[2025-12-14T20:16:00+0100] {errmsg3hdr}"
+        errmsg3_2 = "Which is actually multi line!"
+        print(errmsg3_1, file=fp2); fp2.flush()
+        print(errmsg3_2, file=fp2); fp2.flush()
         print("[2025-12-14T20:17:00+0100] INFO: Ok again.", file=fp2); fp2.flush()
 
     with open(logfiles[2], 'w') as fp3:
         pass
 
-    sleep(0.2)
-    logmon._running = False
+    sleep(0.25)
 
-    with open(logfiles[0], 'a') as fp1, open(logfiles[1], 'a') as fp2:
-        print("[2025-12-14T20:30:00+0100] INFO: EOF", fp1)
-        print("[2025-12-14T20:30:00+0100] INFO: EOF", fp2)
+    proc.terminate()
 
-    #signal.pthread_kill(thread.ident, signal.SIGTERM)
-    thread.join(3)
-    #sleep(3)
+    status = proc.wait()
 
-    if thread_exc is not None:
-        raise thread_exc
+    stdout = proc.stdout.read()
+    stderr = proc.stderr.read()
 
-    mock_smtp = mock_smtp_map.get(('localhost', 587))
-    assert mock_smtp is not None
+    sys.stdout.buffer.write(stdout)
+    sys.stdout.flush()
 
-    assert len(mock_smtp.messages) == 2
-    #for msg in mock_smtp.messages:
-    #    print(msg.get_content())
-    #    print()
+    sys.stderr.buffer.write(stderr)
+    sys.stderr.flush()
 
-    msg = mock_smtp.messages[0]
-    assert msg['Subject'] == '[ERROR] [2025-12-14T20:17:00+0100] ERROR: Something failed!'
-    assert msg['From'] == 'alice@example.com'
-    assert msg['To'] == 'bob@example.com, charly@example.com'
-    content = msg.get_content()
-    assert content == f'''\
-{logfiles[0]}
+    assert proc.returncode == 0
 
-[2025-12-14T20:17:00+0100] ERROR: Something failed!
+    stdout = stdout.decode()
+    stderr = stderr.decode()
 
-[2025-12-14T20:18:00+0100] ERROR: Something else failed!'''
+    expected1 = f'''\
+{logfiles[0]}: Simulate sending email
+> Subject: [ERROR] {errmsg1hdr}\r
+> From: {sender}\r
+> To: {', '.join(receivers)}\r
+> Content-Type: text/plain; charset="utf-8"\r
+> Content-Transfer-Encoding: 7bit\r
+> MIME-Version: 1.0\r
+> \r
+> {logfiles[0]}\r
+> \r
+> {errmsg1}\r
+> \r
+> \r
+> {errmsg2}\r
+> 
+'''
 
-    # TODO: test all messages
+    expected2 = f'''\
+{logfiles[1]}: Simulate sending email
+> Subject: [ERROR] {errmsg3hdr}\r
+> From: {sender}\r
+> To: {', '.join(receivers)}\r
+> Content-Type: text/plain; charset="utf-8"\r
+> Content-Transfer-Encoding: 7bit\r
+> MIME-Version: 1.0\r
+> \r
+> {logfiles[1]}\r
+> \r
+> {errmsg3_1}\r
+> {errmsg3_2}\r
+> 
+'''
+
+    assert expected1 in stderr, f'Message not found in output!\n\n  Message:\n\n{indent(expected1)}\n\n  Output:\n\n{indent(stderr)}'
+    assert expected2 in stderr, f'Message not found in output!\n\n  Message:\n\n{indent(expected2)}\n\n  Output:\n\n{indent(stderr)}'
+
+    for filepath in *logfiles, logmonrc_path:
+        try:
+            os.remove(filepath)
+        except Exception as exc:
+            print(f'Error deleting {filepath}: {exc}')
+
+    proc.stderr.close()
+    proc.stdout.close()
+
+# TODO: test all features
