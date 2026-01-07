3c15fc62e1e60c44a9e0f78a86461e03d2a4b615
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-01-07 21:14:25 +0100

    test oauth2 support

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
-from typing import Optional, TypedDict, Generator, Any
+from typing import Optional, TypedDict, Generator, Any, Literal, NamedTuple, IO
 
 import os
 import sys
 import json
 import pytest
+import pydantic
+import traceback
 
 from os.path import join as join_path
 from tempfile import gettempdir
@@ -12,6 +14,31 @@ from time import sleep
 from subprocess import Popen, PIPE
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
+from base64 import b64encode
+from uuid import uuid4
+from urllib.parse import parse_qsl
+from datetime import datetime, timedelta
+from select import poll, POLLIN, POLLRDHUP, POLLHUP
+
+class OAuthTokenRequestBase(pydantic.BaseModel):
+    scope: Optional[str|list[str]] = None
+
+class OAuthClientCredentialsRequest(OAuthTokenRequestBase):
+    grant_type: Literal['client_credentials']
+    client_id: str
+    client_secret: str
+
+class OAuthPasswordRequest(OAuthTokenRequestBase):
+    grant_type: Literal['password']
+    username: str
+    password: str
+
+class OAuthRefreshTokenRequest(OAuthTokenRequestBase):
+    grant_type: Literal['refresh_token']
+    refresh_token: str
+
+class OAuthTokenRequest(pydantic.BaseModel):
+    request: OAuthClientCredentialsRequest|OAuthPasswordRequest|OAuthRefreshTokenRequest
 
 SRC_PATH = str(Path(__file__).resolve().parent.parent)
 if SRC_PATH not in sys.path:
@@ -98,6 +125,65 @@ def write_logs(logfiles: list[str]) -> Generator[list[ExampleLog], None, None]:
 
     yield []
 
+def pipe_io(stdout: IO[bytes], stderr: IO[bytes]) -> tuple[str, str]:
+    poller = poll()
+    poller.register(stdout.fileno(), POLLIN | POLLRDHUP)
+    poller.register(stderr.fileno(), POLLIN | POLLRDHUP)
+    wait_stdout = True
+    wait_stderr = True
+    prefix = "logmon.py: ".encode()
+    infix = b'\n' + prefix
+
+    stdout_buf = bytearray()
+    stderr_buf = bytearray()
+
+    while wait_stdout or wait_stderr:
+        poll_events = poller.poll(5)
+
+        if not poll_events:
+            break
+
+        for fd, event in poll_events:
+            if fd == stdout.fileno():
+                infile = stdout
+                outfile = sys.stdout
+                buf = stdout_buf
+                if POLLRDHUP & event or POLLHUP & event:
+                    wait_stdout = False
+                    poller.unregister(stdout.fileno())
+                closed = not wait_stdout
+
+            elif fd == stderr.fileno():
+                infile = stderr
+                outfile = sys.stderr
+                buf = stderr_buf
+                if POLLRDHUP & event or POLLHUP & event:
+                    wait_stderr = False
+                    poller.unregister(stderr.fileno())
+                closed = not wait_stderr
+
+            else:
+                assert False
+
+            if closed:
+                rest = infile.read()
+                buf.extend(rest)
+                outfile.buffer.write(prefix)
+                outfile.buffer.write(rest.replace(b'\n', infix))
+                outfile.flush()
+
+            elif POLLIN & event:
+                line = infile.readline()
+                buf.extend(line)
+                outfile.buffer.write(prefix)
+                outfile.buffer.write(line)
+                outfile.flush()
+
+    return (
+        stdout_buf.decode(errors='replace'),
+        stderr_buf.decode(errors='replace'),
+    )
+
 def test_simple(logmonrc_path: str, logfiles: list[str]):
     sender = "alice@example.com"
     receivers = ["bob@example.com", "charly@example.com"]
@@ -139,41 +225,24 @@ logfiles:
     status: Optional[int] = proc.returncode
 
     logs: list[list[ExampleLog]] = []
-    for l in write_logs(logfiles):
-        logs.append(l)
 
-        status: Optional[int] = proc.returncode
-        if status is not None and status != 0:
-            stdout = proc.stdout.read()
-            stderr = proc.stderr.read()
+    try:
+        for l in write_logs(logfiles):
+            logs.append(l)
 
-            sys.stdout.buffer.write(stdout)
-            sys.stdout.flush()
+            status: Optional[int] = proc.returncode
+            if status is not None and status != 0:
+                assert status == 0
 
-            sys.stderr.buffer.write(stderr)
-            sys.stderr.flush()
+        sleep(0.25)
 
-            assert status == 0
+        proc.terminate()
 
-    sleep(0.25)
+        status = proc.wait(5)
+    finally:
+        stdout, stderr = pipe_io(proc.stdout, proc.stderr)
 
-    proc.terminate()
-
-    status = proc.wait()
-
-    stdout = proc.stdout.read()
-    stderr = proc.stderr.read()
-
-    sys.stdout.buffer.write(stdout)
-    sys.stdout.flush()
-
-    sys.stderr.buffer.write(stderr)
-    sys.stderr.flush()
-
-    stdout = stdout.decode()
-    stderr = stderr.decode()
-
-    assert proc.returncode == 0, f"logmon.py error status: {proc.returncode}\n\nstderr:\n{stderr}\n\nstdout:\n{stdout}"
+    assert proc.returncode == 0
 
     expected1 = f'''\
 {logfiles[0]}: Simulate sending email
@@ -214,10 +283,19 @@ logfiles:
     proc.stderr.close()
     proc.stdout.close()
 
-#@pytest.mark.skip("TODO: test_http")
+class TokenData(NamedTuple):
+    client_id: str
+    access_token: str
+    scopes: set[str]
+    expires_at: datetime
+
 def test_http(logmonrc_path: str, logfiles: list[str]):
     sender = "alice@example.com"
     receivers = ["bob@example.com", "charly@example.com"]
+    client_id = str(uuid4())
+    client_secret = b64encode(uuid4().bytes).decode('ISO-8859-1')
+    client_id = "test_id"
+    client_secret = "test_secret"
     logmonrc = f'''\
 ---
 do:
@@ -229,6 +307,12 @@ do:
     receivers: "{{receivers}}"
     entries: "{{...entries}}"
     subject: "{{subject}}"
+  http_timeout: 0.5
+  oauth2_client_id: "{client_id}"
+  oauth2_client_secret: "{client_secret}"
+  oauth2_grant_type: client_credentials
+  oauth2_token_url: http://localhost:8080/oauth/token
+  oauth2_scope: ["write_log"]
   sender: "{sender}"
   receivers:
   - "{receivers[0]}"
@@ -249,16 +333,24 @@ logfiles:
 
     entries: list[Any] = []
     server_errors: list[Exception] = []
+    access_tokens: dict[str, TokenData] = {}
+    token_lifetime = timedelta(hours=1)
 
     def do_illegal_method(self: BaseHTTPRequestHandler) -> None:
         try:
             self.send_response(405)
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(b'')
+            self.wfile.write(json.dumps({
+                "error": "invalid_request",
+                "error_description": f"invalid method: {self.command}"
+            }).encode())
 
             raise Exception(f"unexpected {self.command} request")
         except Exception as exc:
             server_errors.append(exc)
+
+    allowed_scopes = {"write_log", "read_log"}
 
     class Handler(BaseHTTPRequestHandler):
         do_GET     = do_illegal_method
@@ -270,26 +362,184 @@ logfiles:
         do_TRACE   = do_illegal_method
 
         def do_POST(self):
+            headers_sent = False
+            post_body = b''
+            data = None
             try:
-                assert self.path == "/log"
-
                 content_type = self.headers.get('Content-Type')
-                content_len = int(self.headers.get('Content-Length') or '0')
+                content_len = int(self.headers.get('Content-Length') or 0)
                 post_body = self.rfile.read(content_len)
+                self.rfile.close()
 
                 assert content_type is not None
                 content_type = content_type.split(';', 1)[0]
+
+                if self.path == "/oauth/token":
+                    match content_type:
+                        case 'application/x-www-form-urlencoded':
+                            data = { key.decode(): value.decode() for key, value in parse_qsl(post_body) }
+
+                        case 'application/json':
+                            data = json.loads(post_body)
+
+                        case _:
+                            assert content_type in ('application/x-www-form-urlencoded', 'application/json')
+                            raise ValueError(f'illegal Content-Type: {content_type}')
+
+                    request = OAuthTokenRequest(
+                        request=data # type: ignore
+                    ).request
+
+                    if not isinstance(request, OAuthClientCredentialsRequest):
+                        self.send_response(400)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "unsupported_grant_type"
+                        }).encode())
+                        return
+
+                    if request.client_id != client_id or request.client_secret != client_secret:
+                        # not sure if this is the correct error
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "invalid_client"
+                        }).encode())
+                        return
+
+                    scope = request.scope
+                    if scope is not None:
+                        scope_set = set(scope.split() if isinstance(scope, str) else scope)
+                    else:
+                        scope_set = {"read_log"} # default scopes
+
+                    if not allowed_scopes.issuperset(scope_set):
+                        self.send_response(400)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "invalid_request",
+                            "error_description": f"Invalid scopes: {', '.join(sorted(scope_set))}"
+                        }).encode())
+                        return
+
+                    access_token = b64encode(uuid4().bytes).decode('ISO-8859-1')
+                    token = TokenData(
+                        client_id    = client_id,
+                        access_token = access_token,
+                        scopes       = scope_set,
+                        expires_at   = datetime.now() + token_lifetime,
+                    )
+                    access_tokens[access_token] = token
+                    response = json.dumps({
+                        "access_token": access_token,
+                        "token_type": "bearer",
+                        "expires_in": token_lifetime.total_seconds(),
+                    }).encode()
+
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'application/json')
+                    self.end_headers()
+                    headers_sent = True
+                    self.wfile.write(response)
+                    return
+
+                if self.path != "/log":
+                    self.send_response(404)
+                    self.send_header('Content-Type', 'application/json')
+                    self.end_headers()
+                    headers_sent = True
+                    self.wfile.write(json.dumps({
+                        "error": "not_found"
+                    }).encode())
+                    return
+
                 assert content_type == 'application/json'
+
+                if True:
+                    auth_hdr = self.headers.get('Authorization')
+                    if not auth_hdr:
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "unauthorized_client"
+                        }).encode())
+                        return
+
+                    auth_items = auth_hdr.split()
+                    if len(auth_items) != 2 or auth_items[0].lower() != "bearer":
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "unauthorized_client"
+                        }).encode())
+                        return
+
+                    access_token = auth_items[1]
+                    token = access_tokens.get(access_token)
+                    if token is None or token.client_id != client_id:
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "unauthorized_client",
+                            "error_description": "invalid access token"
+                        }).encode())
+                        return
+
+                    if token.expires_at <= datetime.now():
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "invalid_grant",
+                            "error_description": "access token expired"
+                        }).encode())
+                        return
+
+                    if "write_log" not in token.scopes:
+                        self.send_response(401)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+                        headers_sent = True
+                        self.wfile.write(json.dumps({
+                            "error": "invalid_scope",
+                            "error_description": "access token misses write_log scope"
+                        }).encode())
+                        return
 
                 entries.append(json.loads(post_body))
 
                 self.send_response(200)
                 self.send_header('Content-Type', 'application/json')
                 self.end_headers()
-
+                headers_sent = True
                 self.wfile.write('{"success": true}'.encode())
+                self.wfile.write(json.dumps({
+                    "success": True
+                }).encode())
+
             except Exception as exc:
                 server_errors.append(exc)
+                if not headers_sent:
+                    self.send_response(500)
+                    self.send_header('Content-Type', 'application/json')
+                    self.end_headers()
+                self.wfile.write(json.dumps({
+                    "error": "server_error",
+                    "error_description": f"{post_body.decode(errors='replace')}\n\n{data}\n\n{traceback.format_exc()}"
+                }).encode())
 
     server = HTTPServer(('localhost', 8080), Handler)
     thread = Thread(target=lambda: server.serve_forever())
@@ -309,41 +559,24 @@ logfiles:
     status: Optional[int] = proc.returncode
 
     logs: list[list[ExampleLog]] = []
-    for l in write_logs(logfiles):
-        logs.append(l)
 
-        status: Optional[int] = proc.returncode
-        if status is not None and status != 0:
-            stdout = proc.stdout.read()
-            stderr = proc.stderr.read()
+    try:
+        for l in write_logs(logfiles):
+            logs.append(l)
 
-            sys.stdout.buffer.write(stdout)
-            sys.stdout.flush()
+            status: Optional[int] = proc.returncode
+            if status is not None and status != 0:
+                assert status == 0
 
-            sys.stderr.buffer.write(stderr)
-            sys.stderr.flush()
+        sleep(0.5)
 
-            assert status == 0
+        proc.terminate()
 
-    sleep(0.5)
+        status = proc.wait()
+    finally:
+        pipe_io(proc.stdout, proc.stderr)
 
-    proc.terminate()
-
-    status = proc.wait()
-
-    stdout = proc.stdout.read()
-    stderr = proc.stderr.read()
-
-    sys.stdout.buffer.write(stdout)
-    sys.stdout.flush()
-
-    sys.stderr.buffer.write(stderr)
-    sys.stderr.flush()
-
-    stdout = stdout.decode()
-    stderr = stderr.decode()
-
-    assert proc.returncode == 0, f"logmon.py error status: {proc.returncode}\n\nstderr:\n{stderr}\n\nstdout:\n{stdout}"
+    assert proc.returncode == 0
 
     server.shutdown()
     thread.join()
